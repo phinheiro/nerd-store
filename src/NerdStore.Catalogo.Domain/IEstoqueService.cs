908fee1f9ae8b65bac98c272d570d62274d0e788
Pedro Pinheiro <phleitepinheiro@gmail.com>
2021-04-08 18:05:08 -0300

    feat: add serviço de dominio para estoque

---
+++ src/NerdStore.Catalogo.Domain/IEstoqueService.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public interface IEstoqueService : IDisposable
+    {
+        Task<bool> DebitarEstoque(Guid produtoId, int quantidade);
+        Task<bool> ReporEstoque(Guid produtoId, int quantidade);
+    }
+}
