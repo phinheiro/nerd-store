9a48cd7bd358e49f9fee7e8d74e176eb08b0c9cb
Pedro Pinheiro <phleitepinheiro@gmail.com>
2021-04-08 23:23:18 -0300

    feat: implementacao  do mediator

--- src/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -1,3 +1,5 @@
+using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
 using System;
 using System.Threading.Tasks;
 
@@ -6,9 +8,11 @@ namespace NerdStore.Catalogo.Domain
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
-        public EstoqueService(IProdutoRepository produtoRepository)
+        private readonly IMediatrHandler _bus;
+        public EstoqueService(IProdutoRepository produtoRepository, IMediatrHandler bus)
         {
             _produtoRepository = produtoRepository;
+            _bus = bus;
         }
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
         {
@@ -20,7 +24,10 @@ namespace NerdStore.Catalogo.Domain
 
             produto.DebitarEstoque(quantidade);
 
-            // TODO: asdasdad
+            if (produto.QuantidadeEstoque < 10)
+            {
+                await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
+            }
 
             _produtoRepository.Atualizar(produto);
             return await _produtoRepository.UnitOfWork.Commit();
--- src/NerdStore.Catalogo.Domain/Events/ProdutoAbaixoEstoqueEvent.cs
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoAbaixoEstoqueEvent.cs
@@ -6,7 +6,9 @@ namespace NerdStore.Catalogo.Domain.Events
     public class ProdutoAbaixoEstoqueEvent : DomainEvent
     {
         public int QuantidadeRestante { get; private set; }
-        public ProdutoAbaixoEstoqueEvent(Guid aggregateId) : base(aggregateId)
-        { }
+        public ProdutoAbaixoEstoqueEvent(Guid aggregateId, int quantidadeRestante) : base(aggregateId)
+        {
+            QuantidadeRestante = quantidadeRestante;
+        }
     }
 }
