af105848e221cb211a2dcd145a0112e77a84fd34
Pedro Pinheiro <phleitepinheiro@gmail.com>
2021-04-12 01:45:23 -0300

    feat: criado application service de produto
    implementacoes adicionais:
    - mapeamento das entidades com automapper

---
+++ src/NerdStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(d => d.Altura, opt => opt.MapFrom(src => src.Dimensoes.Altura))
+                .ForMember(d => d.Largura, opt => opt.MapFrom(src => src.Dimensoes.Largura))
+                .ForMember(d => d.Profundidade, opt => opt.MapFrom(src => src.Dimensoes.Profundidade));
+
+            CreateMap<Categoria, CategoriaViewModel>();
+        }
+    }
+}
