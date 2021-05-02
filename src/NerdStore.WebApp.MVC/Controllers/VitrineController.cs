bcfab8c68b77b07179d8993cdbd15ec2384616a0
Pedro Pinheiro <phleitepinheiro@gmail.com>
2021-05-01 23:31:09 -0300

    feat: add views e injeção de dependencia do EventHandler

---
+++ src/NerdStore.WebApp.MVC/Controllers/VitrineController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Catalogo.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private readonly IProdutoAppService _produtoAppService;
+        public VitrineController(IProdutoAppService produtoAppService)
+        {
+            _produtoAppService = produtoAppService;
+        }
+
+        [HttpGet("vitrine")]
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _produtoAppService.ObterTodos());
+        }
+
+        [HttpGet("produto-detalhe/{id}")]
+        public async Task<IActionResult> ProdutoDetalhe(Guid id)
+        {
+            return View(await _produtoAppService.ObterPorId(id));
+        }
+    }
+}
--- src/NerdStore.WebApp.MVC/Startup.cs
+++ src/NerdStore.WebApp.MVC/Startup.cs
@@ -72,7 +72,7 @@ namespace NerdStore.WebApp.MVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Vitrine}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
